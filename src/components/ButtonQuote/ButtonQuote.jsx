7219df25d71f5c04104db362509b0a3c886b3911
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-08-13 20:22:43 +0300

    fix

--- src/components/ButtonQuote/ButtonQuote.jsx
+++ src/components/ButtonQuote/ButtonQuote.jsx
@@ -1,14 +1,29 @@
 import styles from './ButtonQuote.module.scss';
-
-
+import { useState } from 'react';
+import FormBackCall from '../FormBackCall/FormBackCall'
 
 
 const ButtonQuote = ({ className, children }) => {
-	
+    const [isFormOpen, setIsFormOpen] = useState(false);
     return (
-        <button className={`${styles.button__quote} ${className}`}>
-            {children}
-        </button>
+        <>
+            {/* Кнопка с вашими стилями */}
+            <button className={`${styles.button__quote} ${className}`} onClick={() => setIsFormOpen(true)}>
+                {children}
+            </button>
+
+            {/* Модальное окно с формой */}
+            {isFormOpen && (
+                <div className={styles.modalOverlay}>
+                    <div className={styles.modalContent}>
+                        <button className={styles.modalClose} onClick={() => setIsFormOpen(false)}>
+                            ×
+                        </button>
+                        <FormBackCall />
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 
