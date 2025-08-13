39ea363f5db7da2984e41faae22dcd36fc4e0948
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-08-13 20:48:48 +0300

    fix

--- src/components/ContactBlock/ContactBlock.jsx
+++ src/components/ContactBlock/ContactBlock.jsx
@@ -1,25 +1,31 @@
 import styles from './ContactBlock.module.scss';
 import { ReactComponent as EmailIcon } from '../../assets/images/email.svg';
 import { ReactComponent as PhoneIcon } from '../../assets/images/phone.svg';
-import IconsDirect from '../IconsDirect/IconsDirect'
+import IconsDirect from '../IconsDirect/IconsDirect';
+
 function ContactBlock({ title, className, children }) {
     return (
         <div className={styles.contacts__container}>
-            <div className={styles.contacts__info}>
-                <div className={styles.contact__row}>
-                    <span className={styles.contact__icon}>
-                        <PhoneIcon className={styles.icon} />
-                    </span>
-                    <span className={styles.contact__text}>Телефон: +7 (123) 456-78-90</span>
+            <div className={styles.contacts__wrapper}>
+                <div className={styles.contacts__info}>
+                    <div className={styles.contact__row}>
+                        <span className={styles.contact__icon}>
+                            <PhoneIcon className={styles.icon} />
+                        </span>
+                        <span className={styles.contact__text}>Телефон: +7 (123) 456-78-90</span>
+                    </div>
+
+                    <div className={styles.contact__row}>
+                        <span className={styles.contact__icon}>
+                            <EmailIcon className={styles.icon} />
+                        </span>
+                        <span className={styles.contact__text}>Почта: example@domain.com</span>
+                    </div>
                 </div>
 
-                <div className={styles.contact__row}>
-                    <span className={styles.contact__icon}>
-                        <EmailIcon className={styles.icon} />
-                    </span>
-                    <span className={styles.contact__text}>Почта: example@domain.com</span>
+                <div className={styles.icons__container}>
+                    <IconsDirect />
                 </div>
-					 <IconsDirect/>
             </div>
 
             <div className={styles.contacts__media}>
