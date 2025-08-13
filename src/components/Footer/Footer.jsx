36e51ba90ee785e63d31d31ebea1d676720e46f9
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-08-13 20:33:54 +0300

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,29 @@
 import styles from './Footer.module.scss';
 import { NavLink } from 'react-router-dom';
 
+// Импортируем SVG иконки (предполагается, что они лежат в папке assets/icons)
+import WhatsAppIcon from '../../assets/images/whatsapp-icon.svg';
+import TelegramIcon from '../../assets/images/telegram-icon.svg';
+import ViberIcon from '../../assets/images/viber-icon.svg';
+
 function Footer() {
     return (
         <footer id="contact" className={styles.footer}>
             <div className={styles.columnLogo}>DTF Promo</div>
+
+            {/* Блок с иконками мессенджеров */}
+            <div className={styles.messengers}>
+                <a href="https://wa.me/78008888" target="_blank" rel="noopener noreferrer">
+                    <img src={WhatsAppIcon} alt="WhatsApp" className={styles.messengerIcon} />
+                </a>
+                <a href="https://t.me/dtfpromo" target="_blank" rel="noopener noreferrer">
+                    <img src={TelegramIcon} alt="Telegram" className={styles.messengerIcon} />
+                </a>
+                <a href="viber://chat?number=78008888" target="_blank" rel="noopener noreferrer">
+                    <img src={ViberIcon} alt="Viber" className={styles.messengerIcon} />
+                </a>
+            </div>
+
             <div className={styles.container}>
                 {/* Левая колонка - Контакты */}
                 <div className={styles.column}>
@@ -14,8 +33,6 @@ function Footer() {
                         <p>Phone: +7 800 88 88</p>
                     </div>
                 </div>
-
-                {/* Центральная колонка - DTF Promo */}
 
                 {/* Правая колонка - Быстрые ссылки */}
                 <div className={styles.column}>
---
+++ src/components/IconsDirect/IconsDirect.jsx
@@ -0,0 +1,13 @@
+import styles from './IconsDirect.module.scss';
+
+
+
+
+const Logo = () => {
+
+    return (
+        
+    );
+};
+
+export default Logo;
