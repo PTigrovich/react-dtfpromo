62ab5b6515482b8bd5af992b05ca2ca9729f384d
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-08-13 20:42:14 +0300

    fix

---
+++ src/components/ContactBlock/ContactBlock.jsx
@@ -0,0 +1,35 @@
+import styles from './ContactBlock.module.scss';
+import { ReactComponent as EmailIcon } from '../../assets/images/email.svg';
+import { ReactComponent as PhoneIcon } from '../../assets/images/phone.svg';
+
+function ContactBlock({ title, className, children }) {
+    return (
+        <div className={styles.contacts__container}>
+            <div className={styles.contacts__info}>
+                <div className={styles.contact__row}>
+                    <span className={styles.contact__icon}>
+                        <PhoneIcon className={styles.icon} />
+                    </span>
+                    <span className={styles.contact__text}>Телефон: +7 (123) 456-78-90</span>
+                </div>
+
+                <div className={styles.contact__row}>
+                    <span className={styles.contact__icon}>
+                        <EmailIcon className={styles.icon} />
+                    </span>
+                    <span className={styles.contact__text}>Почта: example@domain.com</span>
+                </div>
+            </div>
+
+            <div className={styles.contacts__media}>
+                <h3 className={styles.media__title}>Наш офис</h3>
+                <h4 className={styles.media__desription}>
+                    Кремлёвская набережная, 1, Москва, 109012 <br /> Координаты: 55.751442, 37.615569
+                </h4>
+                <img src="/images/location.png" alt="Фото офиса" className={styles.media__image} />
+            </div>
+        </div>
+    );
+}
+
+export default ContactBlock;
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,8 +2,7 @@ import styles from './Contact.module.scss';
 import Layout from '../../components/Layout/Layout';
 import DeliveryBlock from '../../components/DeliveryBlock/DeliveryBlock';
 import FormBackCall from '../../components/FormBackCall/FormBackCall';
-import { ReactComponent as EmailIcon } from '../../assets/images/email.svg';
-import { ReactComponent as PhoneIcon } from '../../assets/images/phone.svg';
+import ContactBlock from '../../components/ContactBlock/ContactBlock';
 
 
 function Contact() {
@@ -17,31 +16,7 @@ function Contact() {
                 </DeliveryBlock>
 
                 <DeliveryBlock className={styles.contact__block} title={'Контактная информация'}>
-                    <div className={styles.contacts__container}>
-                        <div className={styles.contacts__info}>
-                            <div className={styles.contact__row}>
-                                <span className={styles.contact__icon}>
-                                    <PhoneIcon className={styles.icon} />
-                                </span>
-                                <span className={styles.contact__text}>Телефон: +7 (123) 456-78-90</span>
-                            </div>
-
-                            <div className={styles.contact__row}>
-                                <span className={styles.contact__icon}>
-                                    <EmailIcon className={styles.icon} />
-                                </span>
-                                <span className={styles.contact__text}>Почта: example@domain.com</span>
-                            </div>
-                        </div>
-
-                        <div className={styles.contacts__media}>
-                            <h3 className={styles.media__title}>Наш офис</h3>
-                            <h4 className={styles.media__desription}>
-                                 Кремлёвская набережная, 1, Москва, 109012 <br/> Координаты: 55.751442, 37.615569
-                            </h4>
-                            <img src="/images/location.png" alt="Фото офиса" className={styles.media__image} />
-                        </div>
-                    </div>
+                    <ContactBlock/>
                 </DeliveryBlock>
             </div>
         </Layout>
