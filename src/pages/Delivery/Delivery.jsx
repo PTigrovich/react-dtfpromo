cf80b15222490b24490a9a33830675b74a26b053
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-08-14 19:23:15 +0300

    fix

--- src/pages/Delivery/Delivery.jsx
+++ src/pages/Delivery/Delivery.jsx
@@ -10,7 +10,7 @@ function About() {
                     <DeliveryBlock className={styles.bigWrapper__block} title={'Способ доставки'}>
                         <div className={styles.pricing}>
                             <div className={styles.pricing__row}>
-                                <span className={styles.pricing__title}>Базовая</span>
+                                <span className={styles.pricing__title}>Стандартная</span>
                                 <span className={styles.pricing__price}>15$</span>
                             </div>
                             <div className={styles.pricing__row}>
@@ -18,7 +18,7 @@ function About() {
                                 <span className={styles.pricing__price}>20$</span>
                             </div>
                             <div className={styles.pricing__row}>
-                                <span className={styles.pricing__title}>Быстрая</span>
+                                <span className={styles.pricing__title}>Экспресс</span>
                                 <span className={styles.pricing__price}>25$</span>
                             </div>
                         </div>
@@ -26,9 +26,9 @@ function About() {
                     <DeliveryBlock className={styles.bigWrapper__block} title={'Процесс логистики'}>
                         <div className={styles.logistic}>
                             <p>
-                                Наш логистический процесс гарантирует своевременную доставку благодаря отлаженной системе, включающей автоматическую обработку
-                                заказов, отслеживание в режиме реального времени и эффективные каналы дистрибуции. Мы сотрудничаем с проверенными перевозчиками,
-                                чтобы предлагать надёжные услуги.
+                                Наш логистический процесс гарантирует своевременную доставку благодаря отлаженной системе, включающей оперативную автоматическую
+                                обработку заказов, отслеживание в режиме реального времени и эффективные каналы дистрибуции. Мы сотрудничаем с проверенными
+                                перевозчиками, чтобы предлагать надёжные услуги.
                             </p>
                             <div className={styles.logistic__imageWrapper}>
                                 <img src="/images/logistic.png" alt="logistic" className={styles.logisticImage} />
@@ -41,34 +41,36 @@ function About() {
                         <div className={styles.garanty}>
                             <p>
                                 Мы гарантируем своевременную доставку для всех заказов, оформленных до 15:00. Стандартная доставка занимает 5–7 рабочих дней,
-                                экспресс-доставка — 2–3 дня, а доставка на следующий рабочий день.
+                                ускоренная 3-5 дней, экспресс-доставка — 2–3 дня.
                             </p>
                         </div>
                     </DeliveryBlock>
-                    <DeliveryBlock className={styles.middleWrapper__block} title={'Часто спрашивают'}>
+                    <DeliveryBlock className={styles.middleWrapper__block} title={'Вопросы'}>
                         <div className={styles.question}>
                             {/* Блок 1 */}
                             <div className={styles.block}>
-                                <p className={styles.question}>Вопрос 1</p>
-                                <p className={styles.answer}>Ответ на первый вопрос</p>
+                                <p className={styles.question}>Если есть вопросы</p>
+                                <p className={styles.answer}>
+                                    Отправляйте форму или свяжитесь с нами любым удобным способом, наши представители проконсультируют Вас по всем нюансам
+                                    продукта-оплаты-доставки.
+                                </p>
                             </div>
 
                             {/* Блок 2 */}
                             <div className={styles.block}>
-                                <p className={styles.question}>Вопрос 2</p>
-                                <p className={styles.answer}>Ответ на второй вопрос</p>
+                                <p className={styles.question}>Чем профессиональные DTF-принтеры превосходят кустарные</p>
+                                <p className={styles.answer}>
+                                    «Кустарное» DTF-изготовление – высокий брак, заломы изображения и низкая износостойкость Такой продукт проигрывает по всем
+                                    этим параметрам
+                                </p>
                             </div>
 
-                            {/* Блок 3 */}
-                            <div className={styles.block}>
-                                <p className={styles.question}>Вопрос 3</p>
-                                <p className={styles.answer}>Ответ на третий вопрос</p>
-                            </div>
+                            
                         </div>
                     </DeliveryBlock>
                 </div>
                 <DeliveryBlock className={styles.wrapper__smallWrapper} title={'Наши Контакты'}>
-                    <p >
+                    <p>
                         Для получения дополнительной помощи обращайтесь в нашу службу поддержки клиентов по адресу support@dtfdirect.com или звоните нам по
                         телефону (123) 456-7890.
                     </p>
