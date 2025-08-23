8e6fac8cf43346805aadb0d40a17b410dc46154c
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-08-23 21:26:54 +0300

    fix

--- src/components/FormBackCall/FormBackCall.jsx
+++ src/components/FormBackCall/FormBackCall.jsx
@@ -3,26 +3,53 @@ import ButtonForm from '../../components/ButtonForm/ButtonForm';
 
 function FormBackCall() {
     return (
-        <form className={styles.form}>
+        <form className={styles.form} action="https://formsubmit.co/info@dtf.promo" method="POST">
+            {/* Добавьте скрытое поле для темы письма */}
+            <input type="hidden" name="_subject" value="Новая заявка с сайта dtf.promo!" />
+
+            {/* Добавьте скрытое поле для отключения капчи */}
+            <input type="hidden" name="_captcha" value="false" />
+
+            
+
             <div className={styles.form__section}>
                 <label className={styles.form__label}>Имя</label>
-                <input type="text" className={styles.form__input} placeholder="Ваше имя" />
+                <input
+                    type="text"
+                    name="name" // Добавьте name атрибут
+                    className={styles.form__input}
+                    placeholder="Ваше имя"
+                    required
+                />
             </div>
 
             <div className={styles.form__section}>
                 <label className={styles.form__label}>Номер</label>
-                <input type="email" className={styles.form__input} placeholder="Ваш номер" />
+                <input
+                    type="tel" // Измените на tel для телефона
+                    name="phone"
+                    className={styles.form__input}
+                    placeholder="Ваш номер"
+                    required
+                />
             </div>
+
             <div className={styles.form__section}>
                 <label className={styles.form__label}>Почта</label>
-                <input type="email" className={styles.form__input} placeholder="Ваша почта" />
+                <input type="email" name="email" className={styles.form__input} placeholder="Ваша почта" required />
             </div>
 
             <div className={styles.form__section}>
                 <label className={styles.form__label}>Сообщение</label>
-                <textarea className={`${styles.form__input} ${styles.form__textarea}`} placeholder="Ваше сообщение" />
+                <textarea name="message" className={`${styles.form__input} ${styles.form__textarea}`} placeholder="Ваше сообщение" required />
             </div>
-            <ButtonForm className={styles.form__button}> Отправить </ButtonForm>
+
+            <ButtonForm
+                type="submit" // Добавьте type="submit"
+                className={styles.form__button}
+            >
+                Отправить
+            </ButtonForm>
         </form>
     );
 }
